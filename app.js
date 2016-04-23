781f70c09655a4635095792d5bb23e9d108c5ea0
CharlieMcFly <cquetstroey@hotmail.fr>
2016-04-23 13:43:04 +0200

    + Modification du problème lors de l'ajout d'un entrepreneur + Changement du nom d'un entrepreneur en personne mystère

--- app.js
+++ app.js
@@ -19,8 +19,10 @@ app.set('view engine', 'jade');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 // ajout charlie
