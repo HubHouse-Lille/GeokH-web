f47d82737f39827ff9bba9816ee544711ee7ebfe
Narxem <adrien.agez@hotmail.fr>
2017-01-13 16:10:02 +0100

    Mise à jour de la base de données

--- models/parcours.js
+++ models/parcours.js
@@ -11,6 +11,7 @@ module.exports = function(sequelize, DataTypes) {
       associate: function(models) {
         Parcours.hasMany(models.Ptobq),
         Parcours.belongsTo(models.User),
+        Parcours.belongsTo(models.Theme),
         Parcours.hasMany(models.Score)
        }
     }
